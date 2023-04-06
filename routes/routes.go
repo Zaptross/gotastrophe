dd2867a04848bbcdb94dd1c5363172cd0c9f4b28
M. Price <zaptross@outlook.com>
2023-04-07 02:06:28 +1000

    docs: adding AttachRoutes note
    To explain what that function is used for

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// AttachRoutes attaches all routes to the gin engine
+// this allows for simpler local development and easier testing
 func AttachRoutes(r *gin.Engine) {
 	r.GET("/hello-world", GETHelloWorldHandler)
 	r.GET("/go-away", GETGoAwayHandler)
